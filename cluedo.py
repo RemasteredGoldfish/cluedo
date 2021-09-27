a42f8ddc49f9013575b4fcf372af3bbeb4738654
Remastered Goldfish <99067800@mydavinci.nl>
2021-09-27 14:20:04 +0200

    'next'

---
+++ cluedo.py
@@ -0,0 +1,13 @@
+#Questions
+gender = ('What is your gender? ')
+age = ('How old are you? ')
+length = ('how tall are you? ')
+haircolor = ('what is your haircolor? ')
+weight = ('What is your weight? ')
+
+#Setup
+print('Welcome to the online interview. \nAfter this test we can see if you are qualified!\n \nGoodLuck\n')
+
+gender = input(gender)
+
+
