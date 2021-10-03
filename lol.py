326960ca653ca5724632d4c2f870083353cc450c
RemasteredGoldfish <89778483+RemasteredGoldfish@users.noreply.github.com>
2021-10-04 00:55:51 +0200

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,13 @@
+gender = input('What is your gender? ')
+if gender == 'male':
+    length = input('how tall are you in centimeters? ')
+    if length >= '155':
+        weight = int(input('What is your weight? '))
+        if weight <= 99:
+            mustache = input('')
+
+elif gender == 'female':
+        print('okeee')
+        
+else:
+    print('idk wat u are...')
